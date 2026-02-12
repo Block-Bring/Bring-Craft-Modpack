7ce88d4dcee7581ed0a363f4b25925a2adb82114
Block_Bring <144122155+Block-Bring@users.noreply.github.com>
2026-02-12 21:41:13 +0800

    Add l.py
    Some command for migrator.py

---
+++ migrator/l.py
@@ -0,0 +1,16 @@
+import os
+
+# 简化设置标题
+def t(line_title: str):
+    os.system(f"title {line_title}")
+
+# 简化清除命令行
+def c():
+    if os.name == 'nt':
+        os.system('cls')
+    elif os.name == 'posix':
+        os.system('clear')
+
+if __name__ == "__main__":
+    # 不进行任何操作
+    t("Fast Execute")
