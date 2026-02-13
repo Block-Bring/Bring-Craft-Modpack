263ab5b1ba15260a24cfcb925bd6da9613ea0487
Block_Bring <144122155+Block-Bring@users.noreply.github.com>
2026-02-13 23:09:31 +0800

    Update migrator.py
    Preliminarily added the migrate() function and optimized the code's running efficiency.

--- migrator/migrator.py
+++ migrator/migrator.py
@@ -34,7 +34,7 @@ def load_config() -> bool:
     print(f"{YELLOW}正在尝试读取配置文件...{RST}")
     if os.path.exists(CONFIG_FILE):
         try:
-            with open(CONFIG_FILE, 'r', encoding='utf8') as f:
+            with open(CONFIG_FILE, 'r', encoding='utf-8') as f:
                 config = json.load(f)
         except PermissionError:
             print(f"{RED}无法读取配置文件 {CONFIG_FILE}：权限不足。{RST}")
@@ -50,15 +50,15 @@ def load_config() -> bool:
             config_contents = {
                 "minecraft_directory": ""
             }
-            with open(CONFIG_FILE, 'w', encoding='utf8') as f:
-                json.dump(config_contents, f, indent=4)
+            with open(CONFIG_FILE, 'w', encoding='utf-8') as file:
+                json.dump(config_contents, file, indent=4) # type: ignore
                 print(f"{GREEN}成功创建配置文件{RST}")
         except Exception as e:
             print(f"{RED}创建配置文件 {CONFIG_FILE} 失败，原因：{e}")
 
         try:
-            with open(CONFIG_FILE, 'r', encoding='utf8') as f:
-                config = json.load(f)
+            with open(CONFIG_FILE, 'r', encoding='utf-8') as file:
+                config = json.load(file)
         except Exception as e:
             print(f"{RED}无法读取配置文件：{e}")
 
@@ -69,7 +69,6 @@ def load_config() -> bool:
         return False
 # 联机获取信息
 def load_latest_info_file() -> int:
-    global latest
     def request_file() -> bool:
         url = "https://raw.githubusercontent.com/Block-Bring/Bring-Craft-Modpack/refs/heads/main/migrator/latest.json"
         request_headers = {
@@ -92,32 +91,33 @@ def load_latest_info_file() -> int:
         except Exception as exception:
             print(f"在尝试下载最新信息文件时出错：{exception}")
             return False
-    if request_file():
+    def read_latest_file(is_latest):
+        global latest
         if os.path.exists(LATEST_INFO_FILE):
             try:
-                print(f"{YELLOW}正在尝试读取最新信息文件...{RST}")
-                with open(LATEST_INFO_FILE, 'r', encoding='utf-8') as f:
-                    latest = json.load(f)
-            except Exception as e:
-                print(f"{RED}无法读取最新信息文件：{e}")
+                if is_latest:
+                    print(f"{YELLOW}正在尝试读取最新信息文件...{RST}")
+                else:
+                    print(f"{YELLOW}正在尝试读取已有的最新信息文件...{RST}")
+                with open(LATEST_INFO_FILE, 'r', encoding='utf-8') as file:
+                    latest = json.load(file)
+                if is_latest:
+                    print(f"{GREEN}成功加载最新信息文件{RST}")
+                    return 1
+                else:
+                    print(f"{GREEN}成功加载已有的最新信息文件{RST}")
+                    return 2
+            except Exception as exception:
+                print(f"{RED}无法读取最新信息文件：{exception}")
                 return 0
-            print(f"{GREEN}成功加载最新信息文件{RST}")
-            return 1
         else:
             print(f"{RED}最新信息文件不存在{RST}")
             return 0
+
+    if request_file():
+        return read_latest_file(True)
     else:
-        if os.path.exists(LATEST_INFO_FILE):
-            try:
-                print(f"{YELLOW}正在读取已有的最新信息文件...{RST}")
-                with open(LATEST_INFO_FILE, 'r', encoding='utf-8') as f:
-                    latest = json.load(f)
-                print(f"{YELLOW}警告：读取的信息文件可能不是最新的，请仔细甄别！{RST}")
-            except Exception as e:
-                print(f"{RED}无法读取已有的最新信息文件：{e}")
-                return 0
-        print(f"{RED}无法下载最新信息文件{RST}")
-        return 0
+        return read_latest_file(False)
 
 # 判断是否是有效的目录
 def is_valid_minecraft_directory(directory: str) -> bool:
@@ -138,8 +138,8 @@ def is_valid_minecraft_directory(directory: str) -> bool:
 
 
 def select_minecraft_directory(initialize):
-    l.c()
-    l.t("Bring Migrator - 目录选择")
+    l.clear()
+    l.title("Bring Migrator - 目录选择")
     if initialize:
         print(f"{RED}Minecraft 目录无效{RST}")
     else:
@@ -183,7 +183,7 @@ def select_minecraft_directory(initialize):
         config["minecraft_directory"] = directory
         try:
             with open(CONFIG_FILE, "w", encoding="utf-8") as file:
-                json.dump(config, file, indent=4)
+                json.dump(config, file, indent=4) # type: ignore
             print(f"{GREEN}更改完成。请手动重新启动程序。{RST}")
         except Exception as exception:
             print(f"{RED}保存到配置文件 {CONFIG_FILE} 时出错：{exception}{RST}")
@@ -192,11 +192,17 @@ def select_minecraft_directory(initialize):
 
     return select()
 
-def features(is_latest: bool) -> int:
-    l.c()
-    l.t("Bring Migrator - 操作选择")
+def migrate():
+    # 待开发
+    l.clear()
+    l.title("Bring Migrator - 配置迁移")
+    print(f"{RED}该功能正在开发中，敬请期待！{RST}")
+
+def features(latest_is_latest: bool) -> int:
+    l.clear()
+    l.title("Bring Migrator - 操作选择")
     print(f"当前 Minecraft 目录：{GREEN}{dot_minecraft_folder}{RST}")
-    if is_latest:
+    if latest_is_latest:
         print(f"Bring Craft 版本：{GREEN}{modpack_version}{RST}")
     else:
         print(f"Bring Craft 版本：{YELLOW}{modpack_version}（可能过期）{RST}")
@@ -219,31 +225,28 @@ def features(is_latest: bool) -> int:
             print(f"{RED}无效的操作序号{RST}")
     return operating()
 
-
 def main() -> bool:
     global modpack_version, dot_minecraft_folder, feature
-    # 设置窗口标题，使用了 l.py 缩写，名称的扩写为 line.title()
-    l.t("Bring Migrator - 初始化")
+    l.title("Bring Migrator - 初始化")
     loading_config = load_config()
     loading_latest = load_latest_info_file()
+
     if loading_config and loading_latest in (1, 2):
         # 整合包版本定义
         modpack_version = latest["version"]
         if is_valid_minecraft_directory(config["minecraft_directory"]):
             # .minecraft 文件夹
             dot_minecraft_folder = os.path.join(config["minecraft_directory"])
-            if loading_latest == 1:
-                feature = features(True)
-            elif loading_latest == 2:
-                feature = features(False)
+
+            LOADING_LATEST_MAP = {1: True, 2: False}
+            if loading_latest in LOADING_LATEST_MAP:
+                feature = features(LOADING_LATEST_MAP[loading_latest])
             else:
                 return False
-            if feature == 1:
-                pass
-            elif feature == 2:
-                select_minecraft_directory(False)
-            elif feature == 3:
-                pass
+
+            FEATURES_MAP = {1: lambda: migrate(), 2: lambda: select_minecraft_directory(False), 3: lambda: True}
+            if feature in FEATURES_MAP:
+                FEATURES_MAP[feature]()
         else:
             select_minecraft_directory(True)
     else:
@@ -253,12 +256,12 @@ def main() -> bool:
 
 if __name__ == "__main__":
     if main():
-        l.t("Bring Migrator - 正常退出")
+        l.title("Bring Migrator - 正常退出")
         print(f"{GREEN}程序正常退出{RST}")
         input("按 Enter 键结束")
         sys.exit(True)
     else:
-        l.t("Bring Migrator - 异常退出")
+        l.title("Bring Migrator - 异常退出")
         print(f"{RED}程序异常退出{RST}")
         input("按 Enter 键结束")
         sys.exit(False)
