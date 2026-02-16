83f98831b1faecc76047dcabb34d6e881c8c8657
Block_Bring <144122155+Block-Bring@users.noreply.github.com>
2026-02-16 18:33:52 +0800

    Update l.py
    Optimized some parts of the code logic

--- migrator/l.py
+++ migrator/l.py
@@ -2,14 +2,10 @@ import os
 
 # 简化设置标题
 def title(line_title: str):
-    os.system(f"title {line_title}")
+    os.system(f'title {line_title}' if os.name == 'nt' else None)
 
-# 简化清除命令行
 def clear():
-    if os.name == 'nt':
-        os.system('cls')
-    elif os.name == 'posix':
-        os.system('clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 if __name__ == "__main__":
     # 不进行任何操作
