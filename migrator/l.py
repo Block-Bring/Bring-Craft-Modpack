2a15d767f8795ec5edc1edc30bfbc66c839358a5
Block_Bring <144122155+Block-Bring@users.noreply.github.com>
2026-02-18 22:59:46 +0800

    Update l.py
    Added pause(), stop(), and is_url(string)

--- migrator/l.py
+++ migrator/l.py
@@ -1,11 +1,25 @@
 import os
-
+from urllib.parse import urlparse
 # 简化设置标题
 def title(line_title: str):
     os.system(f'title {line_title}' if os.name == 'nt' else None)
 
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def pause():
+    input("按 Enter 键继续...")
+
+def stop():
+    input("按 Enter 键结束")
+
+def is_url(string):
+    try:
+        result = urlparse(string)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+
 
 if __name__ == "__main__":
     # 不进行任何操作
